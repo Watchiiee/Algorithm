9b61afa1727637eed7733259cf6c59d076af8d75
정명성 <op7970@gachon.ac.kr>
2026-03-17 11:40:31 +0900

    [Bronze II] Title: 숫자의 개수, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.cc
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    int arr[10] = { 0, };
+    scanf("%d %d %d", &a, &b, &c);
+    int n = a * b * c;
+
+    int num;
+    while (n > 0) {
+        num = n % 10;
+        arr[num]++;
+        n = n/10;
+    }
+
+    for (int i = 0; i < 10; i++) {
+        printf("%d\n", arr[i]);
+    }
+
+    return 0;
+}
