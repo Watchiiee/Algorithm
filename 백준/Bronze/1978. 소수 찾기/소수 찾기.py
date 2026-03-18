ba1353f8a1ac13b43b0facdf228591acfca8632c
정명성 <op7970@gachon.ac.kr>
2026-03-18 15:16:45 +0900

    [Bronze II] Title: 소수 찾기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,21 @@
+N = int(input())
+
+numbers = list(map(int, input().split()))
+
+prime_count = 0 
+
+for n in numbers:
+    if n == 1:
+        continue
+    
+    is_prime = True
+    
+    for i in range(2, n):
+        if n % i == 0:
+            is_prime = False
+            break
+    
+    if is_prime:
+        prime_count += 1
+
+print(prime_count)
