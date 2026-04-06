ad109d8e88f70c2c692f21a844b3eb66f77520d3
정명성 <op7970@gachon.ac.kr>
2026-04-06 14:59:42 +0900

    [Silver III] Title: Four Squares, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -0,0 +1,20 @@
+import sys
+
+def solve():
+    n = int(sys.stdin.readline())
+    
+    if int(n**0.5)**2 == n:
+        return 1
+    
+    for i in range(1, int(n**0.5) + 1):
+        if int((n - i*i)**0.5)**2 == (n - i*i):
+            return 2
+            
+    for i in range(1, int(n**0.5) + 1):
+        for j in range(1, int((n - i*i)**0.5) + 1):
+            if int((n - i*i - j*j)**0.5)**2 == (n - i*i - j*j):
+                return 3
+                
+    return 4
+
+print(solve())
