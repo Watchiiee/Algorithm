a0eaf9ad84a07cd9733885df58dd458baf28f544
정명성 <op7970@gachon.ac.kr>
2026-04-10 21:34:49 +0900

    [Bronze I] Title: 단어 공부, Time: 76 ms, Memory: 34368 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -0,0 +1,17 @@
+import sys
+
+word = sys.stdin.readline().strip().upper()
+
+unique_words = list(set(word))
+
+cnt_list = []
+
+for x in unique_words:
+    cnt = word.count(x)
+    cnt_list.append(cnt)
+
+if cnt_list.count(max(cnt_list)) > 1:
+    print("?")
+else:
+    max_index = cnt_list.index(max(cnt_list))
+    print(unique_words[max_index])
