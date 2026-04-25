c9317b649d5fd943a072649c94131099e917521f
정명성 <op7970@gachon.ac.kr>
2026-04-25 22:18:57 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,12 @@
+import sys
+
+T = sys.stdin.readline().strip()
+
+if T == "SONGDO":
+    print("HIGHSCHOOL")
+elif T == "CODE":
+    print("MASTER")
+elif T == "2023":
+    print("0611")
+elif T == "ALGORITHM":
+    print("CONTEST")
